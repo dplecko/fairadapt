96d4beb9a150eaff24472cd38de7ee80df4e2637
Drago Plecko <drago.plecko@stat.math.ethz.ch>
2020-11-16 21:41:58 +0100

    Figures fix

--- tests/quantile-methods/Figures-12-13.R
+++ tests/quantile-methods/Figures-12-13.R
@@ -101,7 +101,7 @@ if (experiment == "A") {
 
 }
 
-quant.methods <- c("forest", "linear", "neural network")
+quant.methods <- c("forest", "linear", "nn")
 result <- replicate(length(quant.methods), NULL)
 nrep <- 10
 n.sample <- 5000
