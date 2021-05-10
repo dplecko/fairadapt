07ef59e358d8aca8006e95f0b57f90409ab6e6ed
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2021-05-10 13:46:30 +0200

    file renaming

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,26 @@
+Swap <- function(x, i, j) {
+
+  keep <- x[i]
+  x[i] <- x[j]
+  x[j] <- keep
+
+  return(x)
+
+}
+
+expit <- function(x) return(exp(x)/(1+exp(x)))
+
+MakeLength <- function(x, train.len, full.len) {
+
+  assertthat::assert_that(length(x) %in% c(train.len, full.len))
+  assertthat::assert_that(!is.factor(x))
+
+  if(length(x) != full.len) x <- c(x, rep(NA, full.len - train.len))
+
+  x
+
+}
+
 
 CatOrder <- function(outcome, var) {
 
